6cf676fb3c1d9d87f455fa3c69457383adab8837
Krushna Boinwad <krushnaboinwad70@gmail.com>
2026-01-31 12:46:53 +0530

    add update

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-  mobile: { type: String, required: true, unique: true },
-  name: { type: String, default: "" },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("User", UserSchema);
